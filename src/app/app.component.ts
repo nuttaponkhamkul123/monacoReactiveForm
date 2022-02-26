7c642a2a6ab944df4697462aca06766e60e0d6f0
nuttaponkhamkul123 <aladen0009@gmail.com>
2022-02-27 01:02:57 +0700

    added controlvalueaccessor to monaco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MonacoComponent } from './component/monaco/monaco.component';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,22 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit { 
- 
+  isTab1Ready = false;
+  private _justSetValue: any;
+  private _onChange: any;
+  
   constructor(private fb : FormBuilder){
     
   }
   ngOnInit(): void {
-      console.log((<any>window).monaco);
+    console.log((<any>window).monaco);
       // this.obj = JSON.stringify(this.mainForm);
   }
-  
+  emitVal(value : string) : void{
+    if(value === 'tab 1 ready'){
+      this.isTab1Ready = true;
+    }
+  }
   title = 'monaco';
   
 }
--- src/app/component/monaco/monaco.component.ts
+++ src/app/component/monaco/monaco.component.ts
@@ -1,59 +1,61 @@
-import { AfterViewChecked, AfterViewInit, Component, OnChanges, OnDestroy, OnInit, SimpleChanges, TemplateRef, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+import { AfterViewChecked, AfterViewInit, Component, EventEmitter, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, TemplateRef, ViewChild } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup } from '@angular/forms';
+import { EditorComponent, MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 @Component({
   selector: 'app-monaco',
   templateUrl: './monaco.component.html',
   styleUrls: ['./monaco.component.scss']
 })
-export class MonacoComponent implements OnInit,OnDestroy,AfterViewInit  {
+export class MonacoComponent implements OnInit,OnDestroy , ControlValueAccessor, AfterViewInit {
   mainForm : FormGroup;
   showing : boolean = false;
   epicEditor : any;
+  private _justSetValue: any;
+  private _onChange: any;
+  @ViewChild('monaco') monaco : EditorComponent; 
   options = { theme : 'vs-dark' , language : 'css'}
-  
   constructor(private fb : FormBuilder) { }
-  getEditor(editor : any) : void{
-    this.epicEditor = editor;
-    
+  registerOnChange (fn: any): void {
+    this._onChange = fn;
+    console.log('enter')
+    this.monaco.registerOnChange((val: any) => {
+      if(this._justSetValue === val) {
+        this._justSetValue = undefined;
+        return;
+      }
+      fn(val);
+    });
   }
-  onInit(editor : any) : void{
-    // console.log(editor.value);
-    this.epicEditor = editor;
-    console.log(this.epicEditor.getModel());
-    // editor.dispose();
+
+  registerOnTouched (fn: any): void {
+    this.monaco.registerOnTouched(fn);
+  }
+
+  writeValue (val: any): void {
+    if(this._onChange) {
+      this._onChange(val);
+      this._justSetValue = val;
+    }
+    this.monaco.writeValue(val);
   }
   ngOnDestroy(): void {
-      // this.epicEditor.getModels();
-      // console.log();
-      
-      // console.log(this.mainForm.dirty)
-  }
-  ngOnInit(): void {
-    
-    this.mainForm = new FormGroup({});
-    this.mainForm = this.fb.group({
-      monaco : ['']
-    })
-    console.log(this.showing);
-    console.log(this.mainForm.dirty)
-    // console.log(this.mainForm);
-    // console.log(this.test.elementRef)
-    this.mainForm.reset();
-    if(this.mainForm.dirty === false){
-      console.log('aaa')
-      this.showing = true;
-      console.log('inner : ' , this.mainForm.dirty);
-      
-    }
   }
 
   ngAfterViewInit(): void {
-    this.mainForm.reset();
-    console.log('after view : ' , this.mainForm);
+    this.monaco.registerOnChange(() => {
+      this.mainForm.markAsPristine();
+    })
+  }
+  ngOnInit(): void {
+    this.mainForm = this.fb.group({
+      monaco : ['']
+    })
+
     
   }
+
+
   
 
 }
